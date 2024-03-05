c33ccafdad1fd1c0f411a1b78275a4b6316e0907
Desi-Saitama <devofficial0785@gmail.com>
2024-03-05 23:45:49 +0530

    Running on Spiral Dataset

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import numpy as np
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
 
 X = [[1,2,3,2.5],
 	[2.0, 5.0, -1.0, 2.0],
 	[-1.5, 2.7, 3.3, -0.8]]
+
+X,y = spiral_data(100,3)
 
 class Layer_Dense:
 	def __init__(self, n_inputs, n_neurons):
@@ -22,10 +25,10 @@ class Activation_ReLU:
 
 
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
 #print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)
